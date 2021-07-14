8591fc5f84f319c3619778173b79418f747333af
Diego <diegoteixeirapereira@hotmail.com>
2021-07-14 12:08:06 -0300

    alteração no arquivo classe java de seguranção adicionado habilitação de autenticação.

--- src/main/java/br/com/java/config/security/SecurityConfig.java
+++ src/main/java/br/com/java/config/security/SecurityConfig.java
@@ -9,6 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and();
+        http.cors().and().csrf().disable();
     }
 }
